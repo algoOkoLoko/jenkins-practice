069900f1ca029d41715201aee02021df98dd4b8d
algookoloko <algookoloko@gmail.com>
2026-02-03 14:10:33 -0500

    payment method locator update

--- tests/sep-practice.spec.js
+++ tests/sep-practice.spec.js
@@ -1,66 +1,77 @@
-
 import { test } from "@playwright/test";
 
 test("SEP Practice @sep", async ({ page }) => {
+  // Login:
+  const CREDENTIALS = Buffer.from(
+    `${process.env.SEP_USERNAME}:${process.env.SEP_PASSWORD}`,
+  ).toString("base64");
+  await page.setExtraHTTPHeaders({ Authorization: `Basic ${CREDENTIALS}` });
+  await page.goto(process.env.SEP_QA_URL);
 
-    // Login:
-    const CREDENTIALS = Buffer.from(`${process.env.SEP_USERNAME}:${process.env.SEP_PASSWORD}`).toString("base64");
-    await page.setExtraHTTPHeaders( {Authorization: `Basic ${CREDENTIALS}`} );
-    await page.goto(process.env.SEP_QA_URL);
+  // Start Application Step:
+  let firstNameInputBox = page.locator("//input[@formcontrolname='firstName']");
+  await firstNameInputBox.fill("Muhtar");
 
+  let lastNameInputBox = page.locator("//input[@formcontrolname='lastName']");
+  await lastNameInputBox.fill("Mahmut");
 
-    // Start Application Step:
-    let firstNameInputBox = page.locator("//input[@formcontrolname='firstName']");
-    await firstNameInputBox.fill("Muhtar");
+  let emailInputBox = page.locator("//input[@formcontrolname='email']");
+  await emailInputBox.fill("muhtarmahmut@sep.com");
 
-    let lastNameInputBox = page.locator("//input[@formcontrolname='lastName']");
-    await lastNameInputBox.fill("Mahmut");
+  let phoneNumberInputBox = page.locator(
+    "//input[@formcontrolname='phoneNumber']",
+  );
+  await phoneNumberInputBox.fill("05555555555");
 
-    let emailInputBox = page.locator("//input[@formcontrolname='email']");
-    await emailInputBox.fill("muhtarmahmut@sep.com");
+  let howDidYouHearDropDown = page.locator(
+    "//mat-label[text()='How did you hear about us?']",
+  );
+  await howDidYouHearDropDown.click();
 
-    let phoneNumberInputBox = page.locator("//input[@formcontrolname='phoneNumber']");
-    await phoneNumberInputBox.fill("05555555555");
+  await page.click("//span[text()='Email']");
 
-    let howDidYouHearDropDown = page.locator("//mat-label[text()='How did you hear about us?']");
-    await howDidYouHearDropDown.click();
+  let nextBttn1 = page.locator("//button[text()=' Next']");
+  await nextBttn1.click();
 
-    await page.click("//span[text()='Email']");
+  // Payment Plan Step:
+  let upfrontPaymentPlan = page.locator(
+    "//span[@class='payment-type' and normalize-space()='Upfront']",
+  );
+  await upfrontPaymentPlan.click();
 
-    let nextBttn1 = page.locator("//button[text()=' Next']");
-    await nextBttn1.click();
+  let nextBttn2 = page.locator(
+    "//button[@class='next-button' and text()='Next']",
+  );
+  await nextBttn2.click();
 
-    // Payment Plan Step:
-    let upfrontPaymentPlan = page.locator("//span[@class='payment-type' and normalize-space()='Upfront']");
-    await upfrontPaymentPlan.click();
+  // Review and Submit:
+  let paymentFrame = page.frameLocator(
+    "//iframe[@title='Secure payment input frame']",
+  );
 
-    let nextBttn2 = page.locator("//button[@class='next-button' and text()='Next']");
-    await nextBttn2.click();
+  let cardNumberInputBox = paymentFrame.locator(
+    "//input[@id='payment-numberInput']",
+  );
+  await cardNumberInputBox.fill("4242424242424242");
 
-    // Review and Submit:
-    let paymentFrame = page.frameLocator("//iframe[@title='Secure payment input frame']");
+  let expiryDateInputBox = paymentFrame.locator(
+    "//input[@id='payment-expiryInput']",
+  );
+  await expiryDateInputBox.fill("12/34");
 
-    let cardNumberInputBox = paymentFrame.locator("//input[@id='Field-numberInput']");
-    await cardNumberInputBox.fill("4242424242424242");
+  let cvcInputBox = paymentFrame.locator("//input[@id='payment-cvcInput']");
+  await cvcInputBox.fill("123");
 
-    let expiryDateInputBox = paymentFrame.locator("//input[@id='Field-expiryInput']");
-    await expiryDateInputBox.fill("12/34");
+  let zipCodeInputBox = paymentFrame.locator(
+    "//input[@id='payment-postalCodeInput']",
+  );
+  await zipCodeInputBox.fill("12345");
 
-    let cvcInputBox = paymentFrame.locator("//input[@id='Field-cvcInput']");
-    await cvcInputBox.fill("123");
+  let termsConditionsCheckbox = page.locator("//input[@id='defaultCheck2']");
+  await termsConditionsCheckbox.check();
 
-    let zipCodeInputBox = paymentFrame.locator("//input[@id='Field-postalCodeInput']");
-    await zipCodeInputBox.fill("12345");
+  let payBttn = page.locator("//button[.//span[text()='Pay']]");
+  await payBttn.click();
 
-    let termsConditionsCheckbox = page.locator("//input[@id='defaultCheck2']");
-    await termsConditionsCheckbox.check();
-
-    let payBttn = page.locator("//button[.//span[text()='Pay']]");
-    await payBttn.click();
-
-    await page.waitForTimeout(3000);
-
-
+  await page.waitForTimeout(3000);
 });
-
-
